1fd11cc108fce46a0fa63edfac4fe74da0cfefb3
HyungJu <sungkisa@naver.com>
2023-11-01 13:08:30 +0900

    업뎃

--- app/priceinquiry/[id]/page.tsx
+++ app/priceinquiry/[id]/page.tsx
@@ -11,78 +11,75 @@ type Props = {};
 
 const Chart = dynamic(() => import("@components/Chart"));
 
-function Page({ params }: { params: { id: any } }) {
-  const { data, error } = useSWR("http://10.50.227.158:3000/product", fetcher);
-  if (error) return <div>failed to loading</div>;
-  if (!data) return <div>loading...</div>;
-  console.log(data);
+function Page({params}: { params: { id: any } }) {
+    const {data, error} = useSWR("http://10.50.227.158:3000/product/" + params.id, fetcher);
+    if (error) return <div>failed to loading</div>;
+    if (!data) return <div>loading...</div>;
+    console.log(data);
 
-  return data.map((e: any) => {
-    if (e.id === Number(params.id)) {
-      return (
+    return (
         <div
-          style={{
-            width: "100%",
-            paddingTop: "50px",
-            display: "flex",
-            flexDirection: "column",
-          }}
+            style={{
+                width: "100%",
+                paddingTop: "50px",
+                display: "flex",
+                flexDirection: "column",
+            }}
         >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              padding: "20px",
-            }}
-          >
-            <span
-              style={{ fontSize: "30px", fontWeight: "700", color: "#171717" }}
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: "20px",
+                }}
             >
-              {e ? e.name : "loading"}
-            </span>
             <span
-              style={{ fontSize: "17px", fontWeight: "500", color: "#6B7280" }}
+                style={{fontSize: "30px", fontWeight: "700", color: "#171717"}}
             >
-              {e ? e.category : "loading"}
+              {data ? data.name : "loading"}
             </span>
-          </div>
+                <span
+                    style={{fontSize: "17px", fontWeight: "500", color: "#6B7280"}}
+                >
+              {data ? data.category : "loading"}
+            </span>
+            </div>
 
-          <div
-            style={{
-              display: "flex",
-              backgroundColor: "#E2E8F0",
-              borderRadius: "5px",
-              maxWidth: "100%",
-              alignItems: "center",
-              justifyContent: "space-between",
-              margin: "0 20px 25vh 20px",
-              padding: "20px",
-            }}
-          >
+            <div
+                style={{
+                    display: "flex",
+                    backgroundColor: "#E2E8F0",
+                    borderRadius: "5px",
+                    maxWidth: "100%",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    margin: "0 20px 25vh 20px",
+                    padding: "20px",
+                }}
+            >
             <span
-              style={{
-                color: "#1E293B",
-                fontSize: "15px",
-                fontWeight: "600",
-              }}
+                style={{
+                    color: "#1E293B",
+                    fontSize: "15px",
+                    fontWeight: "600",
+                }}
             >
               평균 시세
             </span>
-            <span
-              style={{
-                color: "#6B7280",
-                fontSize: "15px",
-                fontWeight: "600",
-              }}
-            >
-              {e ? formatNumber(data.priceStat.averagePrice) : 0}원
+                <span
+                    style={{
+                        color: "#6B7280",
+                        fontSize: "15px",
+                        fontWeight: "600",
+                    }}
+                >
+              {data.priceStat ? formatNumber(data.priceStat.averagePrice) : 0}원
             </span>
-          </div>
-          <Chart Prs={e.priceStat.histories} />
+            </div>
+            <Chart Prs={data.priceStat.histories}/>
         </div>
-      );
-    }
-  });
+    );
+
 }
 
 export default Page;
